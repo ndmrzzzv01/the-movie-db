f4320b95686190f1c3c1350f7e98a4ccea4a58ca
Nadushenka Morozova <ndmorozova01@gmail.com>
2022-07-12 14:15:08 +0300

    Fixed Top Rated Movie with MVP.

--- app/src/main/java/com/example/themovies/screens/movie/toprated/TopRatedMoviesFragment.kt
+++ app/src/main/java/com/example/themovies/screens/movie/toprated/TopRatedMoviesFragment.kt
@@ -1,60 +1,63 @@
-//package com.example.themovies.screens.movie.toprated
-//
-//import android.os.Bundle
-//import android.view.LayoutInflater
-//import android.view.View
-//import android.view.ViewGroup
-//import androidx.fragment.app.Fragment
-//import androidx.recyclerview.widget.GridLayoutManager
-//import com.example.themovies.databinding.FragmentMainBinding
-//import com.example.themovies.network.data.Movie
-//import com.example.themovies.screens.likes.data.LikesAdapter
-//import dagger.hilt.android.AndroidEntryPoint
-//import javax.inject.Inject
-//
-//@AndroidEntryPoint
-//class TopRatedMoviesFragment @Inject constructor() : Fragment(),
-//    TopRatedMoviesContract.TopRatedMoviesView {
-//
-//    private lateinit var binding: FragmentMainBinding
-//    @Inject
-//    lateinit var presenter: TopRatedMoviesContract.TopRatedMoviesPresenter
-//    private var adapter = LikesAdapter(mutableListOf(), null)
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View {
-//        binding = FragmentMainBinding.inflate(inflater, container, false)
-//
-//        binding.rvMovies.layoutManager = GridLayoutManager(requireContext(), 2)
-//        binding.rvMovies.adapter = adapter
-//
-//        return binding.root
-//    }
-//
-//    override fun onStart() {
-//        super.onStart()
-//        presenter.loadTopRatedMovies()
-//    }
-//
-//    override fun onStop() {
-//        super.onStop()
-//        presenter.cancel()
-//    }
-//
-//    override fun displayListOfTopRatedMovies(list: List<Movie>) {
-//        binding.rvMovies.visibility = View.VISIBLE
-//        adapter.list = list
-//        adapter.notifyDataSetChanged()
-//    }
-//
-//    override fun onFail() {
-//        binding.apply {
-//            rvMovies.visibility = View.INVISIBLE
-//            btnRetry.visibility = View.VISIBLE
-//            tvError.visibility = View.VISIBLE
-//        }
-//    }
-//}
+package com.example.themovies.screens.movie.toprated
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.themovies.databinding.FragmentMainBinding
+import com.example.themovies.network.data.Movie
+import com.example.themovies.screens.likes.data.LikesAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class TopRatedMoviesFragment : Fragment(),
+    TopRatedMoviesContract.TopRatedMoviesView {
+
+    private lateinit var binding: FragmentMainBinding
+
+    @Inject
+    lateinit var presenter: TopRatedMoviesPresenter
+    private var adapter = LikesAdapter(mutableListOf(), null)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+
+        binding.rvMovies.layoutManager = GridLayoutManager(requireContext(), 2)
+        binding.rvMovies.adapter = adapter
+
+        presenter.setView(this)
+
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        presenter.loadTopRatedMovies()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        presenter.cancel()
+    }
+
+    override fun displayListOfTopRatedMovies(list: List<Movie>) {
+        binding.rvMovies.visibility = View.VISIBLE
+        adapter.list = list
+        adapter.notifyDataSetChanged()
+    }
+
+    override fun onFail() {
+        binding.apply {
+            rvMovies.visibility = View.INVISIBLE
+            btnRetry.visibility = View.VISIBLE
+            tvError.visibility = View.VISIBLE
+        }
+    }
+}
